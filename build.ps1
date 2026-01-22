b770160e78118ade8250decca0d381ef7f538516
Javier Pimás <elpochodelagente@gmail.com>
2026-01-22 14:39:31 -0300

    Update build scripts to unify build directory naming and improve CMake configuration

--- build.ps1
+++ build.ps1
@@ -10,11 +10,15 @@ if (-not $VcpkgRoot) {
 }
 
 $Toolchain = "$VcpkgRoot/scripts/buildsystems/vcpkg.cmake"
+$Prefix = "$VcpkgRoot/installed/$Triplet"
 
-cmake -S . -B build-win32 -G "Visual Studio 17 2022" -A Win32 `
+cmake -S . -B build-win32 `
+    -G "Visual Studio 17 2022" `
+    -A Win32 `
     -DCMAKE_MSVC_RUNTIME_LIBRARY=MultiThreaded `
     -DCMAKE_TOOLCHAIN_FILE="$Toolchain" `
-    -DVCPKG_TARGET_TRIPLET="$Triplet" `
+    -DVCPKG_TARGET_TRIPLET=$Triplet `
+    -DCMAKE_PREFIX_PATH="$Prefix" `
     -DCMAKE_FIND_PACKAGE_PREFER_CONFIG=ON `
     -DBUILD_TESTING=OFF `
     -DCURL_USE_SCHANNEL=ON `
@@ -28,4 +32,5 @@ cmake -S . -B build-win32 -G "Visual Studio 17 2022" -A Win32 `
     -DBUILD_STATIC_CURL=ON `
     -DCURL_STATIC_CRT=ON `
     -DCURL_STATIC_DEPS=ON
+
 cmake --build build-win32 --config Release
